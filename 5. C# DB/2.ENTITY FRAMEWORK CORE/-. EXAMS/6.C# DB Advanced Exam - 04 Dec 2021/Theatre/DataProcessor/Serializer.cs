f53ad08a30837e3cf9e23a69c1c57ad7bd94c7ef
Mariyan <116071995+myro97@users.noreply.github.com>
2023-11-19 13:36:15 +0200

    Update 5. C# DB\2.ENTITY FRAMEWORK CORE\-. EXAMS
    Update 6.C# DB Advanced Exam - 04 Dec 2021
    -> Add 4.	Data Export (25 pts)
    - Add ExportPlayDto
    - Add ExportActorDto
    - Update Serializer

--- 5. C# DB/2.ENTITY FRAMEWORK CORE/-. EXAMS/6.C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/Serializer.cs
+++ 5. C# DB/2.ENTITY FRAMEWORK CORE/-. EXAMS/6.C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/Serializer.cs
@@ -1,19 +1,72 @@
 ﻿namespace Theatre.DataProcessor
 {
-
+    using CarDealer.Utilities;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
     using System;
+    using System.Xml.Linq;
     using Theatre.Data;
+    using Theatre.Data.Models;
+    using Theatre.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
-            throw new NotImplementedException();
+            var theatres = context.Theatres
+                .AsNoTracking()
+                .Where(t => t.NumberOfHalls >= numbersOfHalls
+                        && t.Tickets.Count >= 20)
+                .OrderByDescending(t => t.NumberOfHalls)
+                .ThenBy(t => t.Name)
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Halls = t.NumberOfHalls,
+                    TotalIncome = t.Tickets
+                        .Where(t => t.RowNumber >= 1 && t.RowNumber <= 5)
+                        .Sum(t => t.Price),
+                    Tickets = t.Tickets
+                        .Where(t => t.RowNumber >= 1 && t.RowNumber <= 5)
+                        .OrderByDescending(t => t.Price)
+                        .Select(t => new
+                        {
+                            Price = t.Price,
+                            RowNumber = t.RowNumber
+                        })
+                        .ToArray()
+                })
+                .ToArray();
+
+                return JsonConvert.SerializeObject(theatres, Formatting.Indented);
         }
 
         public static string ExportPlays(TheatreContext context, double raiting)
         {
-            throw new NotImplementedException();
+            ExportPlayDto[] playDtos = context.Plays
+                .Where(p => p.Rating <= raiting)
+                .ToArray()
+                .Select(p => new ExportPlayDto
+                {
+                    Title = p.Title,
+                    Duration = p.Duration.ToString("c"),
+                    Rating = p.Rating == 0.0 ? "Premier" : p.Rating.ToString(),
+                    Genre = p.Genre.ToString(),
+                    Actors = p.Casts
+                        .Where(c => c.IsMainCharacter)
+                        .OrderByDescending(c => c.FullName)
+                        .Select(c => new ExportActorDto
+                        {
+                            FullName = c.FullName,
+                            MainCharacter = $"Plays main character in '{p.Title}'."
+                        })
+                        .ToArray()
+                })
+                .OrderBy(p => p.Title)
+                .ThenByDescending(p => p.Genre)
+                .ToArray();
+
+            return new XmlHelper().Serialize(playDtos, "Plays");
         }
     }
 }
