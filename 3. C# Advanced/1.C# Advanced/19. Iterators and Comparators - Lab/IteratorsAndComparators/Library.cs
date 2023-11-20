ef0f2023d96137b47ae8a12de8b1708f8a5675a9
Mariyan <116071995+myro97@users.noreply.github.com>
2023-11-20 13:36:17 +0200

    Update 3. C# Advanced\1.C# Advanced
    Add 19. Iterators and Comparators - Lab

---
+++ 3. C# Advanced/1.C# Advanced/19. Iterators and Comparators - Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        class LibraryIterator : IEnumerator<Book>
+        {
+
+            private int currentIndex;
+            private readonly List<Book> books;
+
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public void Dispose() { }
+
+            public bool MoveNext() => ++this.currentIndex < this.books.Count;
+
+            public void Reset() => this.currentIndex = -1;
+            public Book Current => this.books[currentIndex];
+            object IEnumerator.Current => Current;
+        }
+    }
+}
