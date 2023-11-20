e53622e08c0801076f791e3310923c50522dbcca
Mariyan <116071995+myro97@users.noreply.github.com>
2023-11-20 13:15:47 +0200

    Update 3. C# Advanced\1.C# Advanced
    Add 15. Implementing Linked List

---
+++ 3. C# Advanced/1.C# Advanced/15. Implementing Linked List/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+////List<int> ren = new();
+
+
+//using System.Linq;
+
+//LinkedList<int> linkedList = new();
+//linkedList.AddFirst(1);
+//linkedList.AddFirst(2);
+//linkedList.AddFirst(3);
+//linkedList.AddFirst(4);
+//linkedList.AddFirst(5);
+
+//linkedList.AddLast(6);
+//linkedList.AddLast(7);
+//linkedList.AddLast(8);
+//linkedList.AddLast(9);
+//linkedList.AddLast(10);
+
+//LinkedListNode<int> node = linkedList.Last;
+
+//while (node != null)
+//{
+//    Console.WriteLine(node.Value);
+//    node = node.Previous;
+//}
+
+
+///////////////////////////////////
+//LinkedList<int> stack = new();
+
+//stack.AddFirst(1);
+//stack.AddFirst(2);
+//stack.AddFirst(3);
+
+
+//while(stack.Count() > 0)
+//{
+//    Console.WriteLine(stack.First.Value);
+//    stack.RemoveFirst();
+//}
+
+///////////////////////////////////
+//using Implementing_Linked_List;
+
+//Node node = new(1);
+//Node node1 = new(2);
+//Node node2 = new(3);
+//Node node3 = new(4);
+//Node node4 = new(5);
+//Node node5 = new(6);
+
+//node.Next = node1;
+//node1.Next = node2;
+//node2.Next = node3;
+//node3.Next = node4;
+//node4.Next = node5;
+
+//Node current = node;
+
+//while(current != null)
+//{
+//    Console.WriteLine(current.Value);
+//    current = current.Next;
+//}
+/////////////////////////////////////////
+
+
+using Implementing_Linked_List;
+
+SoftUniLinkedList softUniLinkedList = new();
+
+softUniLinkedList.AddLast(1);
+softUniLinkedList.AddLast(2);
+softUniLinkedList.AddLast(3);
+softUniLinkedList.AddLast(4);
+softUniLinkedList.AddLast(5);
+softUniLinkedList.AddLast(6);
+
+softUniLinkedList.AddFirst(51);
+
+int[] arr = softUniLinkedList.ToArray();
+Console.WriteLine(String.Join(", ", arr));
+
+softUniLinkedList.RemoveFirst();
+softUniLinkedList.RemoveLast();
+
+Node node = softUniLinkedList.Head;
+
+//while(node != null)
+//{
+//    Console.WriteLine(node.Value);
+//    node = node.Next;
+//}
+
+softUniLinkedList.ForEach(x => Console.WriteLine($"Node: {x}"));
---
+++ 3. C# Advanced/1.C# Advanced/15. Implementing Linked List/SoftUniLinkedList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Linked_List
+{
+    internal class SoftUniLinkedList
+    {
+        public Node Head { get;set; }
+
+        public Node Tail { get;set; }
+        public int Count { get; set; }
+
+        //methods
+        public void AddFirst(int value)
+        {
+            Count++;
+            Node node = new(value);
+            if (this.Head == null)
+            {
+                this.Head = node;
+                this.Tail = node;
+                return;
+            }
+
+            node.Next = Head;
+            this.Head.Previous = node;
+            this.Head = node;
+        }
+
+        public void AddLast(int value)
+        {
+            Count++;
+            Node node = new(value);
+            if(this.Tail == null)
+            {
+                this.Tail = node;
+                this.Head = node;
+                return;
+            }
+
+            node.Previous = this.Tail;
+            this.Tail.Next = node;
+            this.Tail = node;
+        }
+
+        public void RemoveFirst()
+        {
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException("The LinledList is empty");
+            }
+            if(Head.Next == null)
+            {
+                Head = null;
+                Tail = null;
+                //Count--;
+            }
+            else
+            {
+                Node oldHead = this.Head;
+                Head = Head.Next;
+
+                Head.Previous = null;
+                oldHead.Next = null;
+            }
+            Count--;   
+        }
+
+        public void RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The LinledList is empty");
+            }
+            if (Tail.Previous == null)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else
+            {
+                Node oldTail = this.Tail;
+                this.Tail = Tail.Previous;
+
+                Tail.Next = null;
+                oldTail.Previous = null;
+            }
+            Count--;
+        }
+
+        public void ForEach(Action<int> callBack)
+        {
+            Node current = Head;
+            while(current != null)
+            {
+                callBack(current.Value);
+                current = current.Next;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+            int i = 0;
+            ForEach(e => array[i++] = e);
+
+            return array;
+        }
+    }
+}
