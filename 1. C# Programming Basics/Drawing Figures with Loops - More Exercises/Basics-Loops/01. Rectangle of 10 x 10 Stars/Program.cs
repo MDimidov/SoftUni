1c9de1576c3eca67a5b32b0007cd1d443308b226
Mariyan <116071995+myro97@users.noreply.github.com>
2023-11-22 10:02:07 +0200

    Update 1. C# Programming Basics\
    Update Drawing Figures with Loops - More Exercises
    - Basics-Loops

---
+++ 1. C# Programming Basics/Drawing Figures with Loops - More Exercises/Basics-Loops/01. Rectangle of 10 x 10 Stars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Rectangle_of_10_x_10_Stars
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 1. C# Programming Basics/Drawing Figures with Loops - More Exercises/Basics-Loops/08. Sunglasses/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Sunglasses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                //first glass
+                if (i == 0 || i == n - 1)
+                {
+                    for (int j = 1; j <= n * 2; j++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                else
+                {
+                    Console.Write('*');
+                    for (int j = 1; j <= n * 2 - 2; j++)
+                    {
+                        Console.Write('/');
+                    }
+                    Console.Write('*');
+                }
+
+                //middle
+                if ((n - 1) / 2 - 1 == i - 1)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                else
+                {
+                    for(int j = 0; j < n; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                //second glasss
+                if (i == 0 || i == n - 1)
+                {
+                    for (int j = 1; j <= n * 2; j++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                else
+                {
+                    Console.Write('*');
+                    for (int j = 1; j <= n * 2 - 2; j++)
+                    {
+                        Console.Write('/');
+                    }
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
