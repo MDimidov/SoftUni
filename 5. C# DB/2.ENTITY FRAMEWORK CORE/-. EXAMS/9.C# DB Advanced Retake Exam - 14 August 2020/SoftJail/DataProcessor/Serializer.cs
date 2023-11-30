ff5f1e4380476aab928acfe36c0c1ca54493b1b6
Mariyan <116071995+myro97@users.noreply.github.com>
2023-11-30 17:39:04 +0200

    Update 5. C# DB\2.ENTITY FRAMEWORK CORE\-. EXAMS
    Update 9.C# DB Advanced Retake Exam - 14 August 2020
    -> Add 3. Data Export (20 Pts)
    JSON export
    - Update Serializer

--- 5. C# DB/2.ENTITY FRAMEWORK CORE/-. EXAMS/9.C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
+++ 5. C# DB/2.ENTITY FRAMEWORK CORE/-. EXAMS/9.C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
@@ -1,12 +1,39 @@
 ﻿namespace SoftJail.DataProcessor
 {
     using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using SoftJail.Data.Models;
+    using System.Text.Json.Nodes;
 
     public class Serializer
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            throw new NotImplementedException();
+            var prisonersByCells = context.Prisoners
+                .AsNoTracking()
+                .Where(p => ids.Contains(p.Id))
+                .OrderBy(p => p.FullName)
+                .ThenBy(p => p.Id)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers
+                        .OrderBy(po => po.Officer.FullName)
+                        .Select(po => new
+                        {
+                            OfficerName = po.Officer.FullName,
+                            Department = po.Officer.Department.Name
+                        })
+                        .ToArray(),
+                    TotalOfficerSalary = Math.Round(p.PrisonerOfficers
+                        .Sum(po => po.Officer.Salary), 2)
+                })
+                .ToArray();
+
+            return JsonConvert.SerializeObject(prisonersByCells, Formatting.Indented);
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
