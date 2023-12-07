739a6f8a10bbacbe00806105edc748ae798da43b
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-07 11:39:58 +0200

    Update 4. JS Front-End\2. JS Front-End\
    Update 9. DOM and Events
    -> Add 03. Collect-List-Items

---
+++ 4. JS Front-End/2. JS Front-End/9. DOM and Events/03. Collect-List-Items/CollectListItems.js
@@ -0,0 +1,6 @@
+function extractText() {
+  const itemsList = Array.from(document.querySelectorAll("li"));
+
+  const result = itemsList.map((item) => item.textContent).join("\n");
+  document.querySelector("#result").value = result;
+}
