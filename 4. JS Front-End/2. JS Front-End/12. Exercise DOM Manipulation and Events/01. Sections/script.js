4acbb81275fd99c4c415135776eb3577d4c1b56c
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-11 13:42:01 +0200

    Update 4. JS Front-End\2. JS Front-End\
    Add 12. Exercise DOM Manipulation and Events
    -> Add 01. Sections

---
+++ 4. JS Front-End/2. JS Front-End/12. Exercise DOM Manipulation and Events/01. Sections/script.js
@@ -0,0 +1,18 @@
+function create(words) {
+   const mainDiv = document.querySelector("#content");
+
+   for (const word of words) {
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+
+      p.textContent = word;
+      p.style.display = "none";
+
+      div.appendChild(p);
+      mainDiv.appendChild(div);
+
+      div.addEventListener("click", () => {
+         p.style.display = '';
+      });
+   }
+}
