343718d96476f25c34a005f5ee53074e573cf44f
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-22 18:12:06 +0200

    Update 7. C# DataStructures\1.DataStructuresFundamentals\
    Update 4. Linear Data Structures
    -> Implemented List - need to test

--- 7. C# DataStructures/1.DataStructuresFundamentals/4. Linear Data Structures/02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem01.List/List.cs
+++ 7. C# DataStructures/1.DataStructuresFundamentals/4. Linear Data Structures/02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem01.List/List.cs
@@ -10,56 +10,132 @@
         private T[] items;
 
         public List()
-            : this(DEFAULT_CAPACITY) {
+            : this(DEFAULT_CAPACITY)
+        {
         }
 
         public List(int capacity)
         {
-            throw new NotImplementedException();
+            if (capacity < 0)
+            {
+                throw new NotImplementedException(capacity);
+            }
+
+            this.items = new T[capacity];
         }
 
-        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return this.items[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+                this.items[index] = value;
+            }
+        }
 
         public int Count { get; private set; }
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            Grow();
+
+            this.items[this.Count++] = item;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            if (IndexOf(item) != -1)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            foreach (var item in this.items.Take(this.Count))
+            {
+                yield return item;
+            }
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (item = this.items[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            ValidateIndex(index);
+            Grow();
+
+            for (int i = this.Count - 1; i >= index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+
+            this.items[index] = item;
+            this.Count++;
+
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            int index = IndexOf(item);
+            RemoveAt(index);
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            ValidateIndex(index);
+
+            for (int i = index; i < this.Count; i--)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[this.Count] = default;
+            this.Count--;
+
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void ValidateIndex(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException("Index is outside the bounds!");
+            }
+        }
+
+        private void Grow()
+        {
+            if (this.items.length <= this.Count)
+            {
+                T[] coppyArray = new T[this.items.length * 2];
+                for (int i = 0; i < items.length; i++)
+                {
+                    coppyArray[i] = this.items[i];
+                }
+
+                this.items = coppyArray;
+            }
         }
     }
 }
