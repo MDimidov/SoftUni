eb08bd31c56b8996afb8d2b728cf3eba53a03c60
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-23 19:23:18 +0200

    Update 7. C# DataStructures\1.DataStructuresFundamentals\
    Update 4. Linear Data Structures
    -> Implemented LinkedList - need to test

--- 7. C# DataStructures/1.DataStructuresFundamentals/4. Linear Data Structures/02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ 7. C# DataStructures/1.DataStructuresFundamentals/4. Linear Data Structures/02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -6,46 +6,111 @@
 
     public class SinglyLinkedList<T> : IAbstractLinkedList<T>
     {
-        public int Count => throw new NotImplementedException();
+        private class Node()
+        {
+            public Node(T elemennt)
+            {
+                this.Element = elemennt;
+            }
+
+            public Node(T elemennt, Node next)
+            {
+                this.Element = elemennt;
+                this.Next = next;
+            }
+
+            public T Element { get; set; }
+            public Node Next { get; set; }
+        }
+
+        private Node head;
+
+        public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            Node node = new Node(item, this.head);
+            head = node;
+            Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(item);
+            Node node = this.head;
+
+            while(node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            node.Next = newNode;
+            Count++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            IsListEmpty();
+            Node node = this.head;
+            while(node != null)
+            {
+                yield return node;
+                node = node.Next;
+            }
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            IsListEmpty();
+            return this.head.Element;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            IsListEmpty();
+            Node node = this.head;
+            while(node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            return node.Element;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            IsListEmpty();
+            Node oldHead = this.head;
+            this.head = oldHead.Next;
+            Count--;
+
+            return oldHead.Element;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            Node node = this.head;
+
+            while(node.Next.Next != null)
+            {
+                node = node.Next;
+            }
+
+            T element = node.Next.Element;
+            node.Next = null;
+            Count--;
+            return element;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void IsListEmpty()
         {
-            throw new NotImplementedException();
+            if(this.Count <= 0)
+            {
+                throw new InvalidOperationException("The linked list is empty");
+            }
         }
     }
 }
