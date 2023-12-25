16bf8f16c4218c95450159079d18d6e77036a148
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-25 19:08:40 +0200

    Update 7. C# DataStructures\1.DataStructuresFundamentals\
    Update 5. Exercise Linear Data Structures
    -> Implemented DoublyLinkedList - need to test

--- 7. C# DataStructures/1.DataStructuresFundamentals/5. Exercise Linear Data Structures/02.DoublyLinkedList/DoublyLinkedList.cs
+++ 7. C# DataStructures/1.DataStructuresFundamentals/5. Exercise Linear Data Structures/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -6,46 +6,135 @@
 
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
+
+        private class Node
+        {
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+
+            public T Element { get; private set; }
+
+            public Node Previous { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        private Node head;
+        private Node tail;
+
+        public DoublyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.Count = 0;
+        }
+
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(item);
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                this.head.Previous = newNode;
+                newNode.Next = this.head;
+                this.head = newNode;
+            }
+
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(item);
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                this.tail.Next = newNode;
+                newNode.Previous = this.tail;
+                this.tail = newNode;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            ValidateList();
+            return this.head.Element;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            ValidateList();
+            return this.tail.Element;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            ValidateList();
+            T firstElement = this.head.Element;
+
+            if (this.head.Next == null)
+            {
+                this.head = this.tail = null;
+            }
+            else
+            {
+                this.head = this.head.Next;
+                this.head.Previous = null;
+            }
+
+            Count--;
+            return firstElement;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            ValidateList();
+            T lastElement = this.tail.Element;
+            if (this.tail.Previous == null)
+            {
+                this.head = this.tail = null;
+            }
+            else
+            {
+                this.tail = this.tail.Previous;
+                this.tail.Next = null;
+            }
+
+            Count--;
+            return lastElement;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node currentNode = this.head;
+            while (currentNode != null)
+            {
+                yield return currentNode.Element;
+                currentNode = currentNode.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void ValidateList()
         {
-            throw new NotImplementedException();
+            if (this.Count <= 0)
+            {
+                throw new InvalidOperationException("Doubly Linked List should not be empty!");
+            }
         }
     }
 }
