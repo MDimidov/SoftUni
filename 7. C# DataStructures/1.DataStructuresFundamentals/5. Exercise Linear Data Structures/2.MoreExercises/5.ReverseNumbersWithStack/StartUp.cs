8aff048b2d2a1086e209b298fb3ac94d2dbcca94
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-27 15:51:45 +0200

    Update 7. C# DataStructures\1.DataStructuresFundamentals\
    Update 5. Exercise Linear Data Structures\
    -> Add 2.MoreExercises
    - Add 5.ReverseNumbersWithStack

---
+++ 7. C# DataStructures/1.DataStructuresFundamentals/5. Exercise Linear Data Structures/2.MoreExercises/5.ReverseNumbersWithStack/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace AnotherTest
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new();
+
+            foreach (int num in inputArr)
+            {
+                stack.Push(num);
+            }
+
+            for(int i = 0; i < inputArr.Length; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
