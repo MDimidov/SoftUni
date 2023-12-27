dabf6342bd707a75a9e73c06fb9f823960a71d34
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-27 16:03:18 +0200

    Update 7. C# DataStructures\1.DataStructuresFundamentals\
    Update 5. Exercise Linear Data Structures\
    -> Update 2.MoreExercises
    - Add 7.SequenceNtoM -> not ready

---
+++ 7. C# DataStructures/1.DataStructuresFundamentals/5. Exercise Linear Data Structures/2.MoreExercises/7.SequenceNtoM/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace test
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Console.ReadLine()!
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = nm[0];
+            int m = nm[1];
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(n);
+
+            while (queue.Any())
+            {
+                int e = queue.Dequeue();
+                queue.Enqueue(e + 1);
+                queue.Enqueue(e + 2);
+                queue.Enqueue(e * 2);
+
+                if (n > m)
+                {
+                    Console.WriteLine("(no solution)");
+                    return;
+                }
+                else if(e == m)
+                {
+                    Console.Write(e);
+                    return;
+                }
+                else
+                {
+                    Console.Write(e + " -> ");
+                }
+            }
+        }
+    }
+}
