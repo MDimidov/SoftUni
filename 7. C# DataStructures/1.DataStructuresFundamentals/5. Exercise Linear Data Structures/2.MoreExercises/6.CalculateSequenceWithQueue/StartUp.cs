7792d8e60e8aaa438c2a1b9c911f19a29fb6e139
Mariyan <116071995+myro97@users.noreply.github.com>
2023-12-27 15:53:39 +0200

    Update 7. C# DataStructures\1.DataStructuresFundamentals\
    Update 5. Exercise Linear Data Structures\
    -> Update 2.MoreExercises
    - Add 6.CalculateSequenceWithQueue

---
+++ 7. C# DataStructures/1.DataStructuresFundamentals/5. Exercise Linear Data Structures/2.MoreExercises/6.CalculateSequenceWithQueue/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace test
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(n);
+
+            for (int i = 1; i <= 50; i++)
+            {
+                int s = queue.Dequeue();
+                queue.Enqueue(s + 1);
+                queue.Enqueue(2 * s + 1);
+                queue.Enqueue(s + 2);
+
+                if (i < 50)
+                {
+                    Console.Write(s + ", ");
+                }
+                else
+                {
+                    Console.Write(s);
+                }
+            }
+        }
+    }
+}
