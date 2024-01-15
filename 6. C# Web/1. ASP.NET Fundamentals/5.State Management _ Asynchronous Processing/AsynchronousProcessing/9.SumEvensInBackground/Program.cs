f81e29a07aad5cc45e566ba1b17e8498126f1950
Mariyan <116071995+myro97@users.noreply.github.com>
2024-01-15 22:17:58 +0200

    Update 6. C# Web\1. ASP.NET Fundamentals\
    Update 5.State Management _ Asynchronous Processing/AsynchronousProcessing/
    -> Add 9.SumEvensInBackground

---
+++ 6. C# Web/1. ASP.NET Fundamentals/5.State Management _ Asynchronous Processing/AsynchronousProcessing/9.SumEvensInBackground/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace _9.SumEvensInBackground;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        long sum = 0;
+
+        Task task = Task.Run(() =>
+        {
+            sum = 0;
+            for (long i = 0; i < 10000000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+                Console.WriteLine(sum);
+            }
+        });
+
+        while (true)
+        {
+            string line = Console.ReadLine()!;
+            if (line == "exit")
+            {
+                return;
+            }
+            else if (line == "show")
+            {
+                Console.WriteLine("true");
+            }
+        }
+    }
+}
