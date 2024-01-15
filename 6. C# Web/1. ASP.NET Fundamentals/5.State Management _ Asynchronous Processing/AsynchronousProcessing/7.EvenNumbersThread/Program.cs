e643cd9eb4c20ca0fac07cf0abdf9d53f2934cd7
Mariyan <116071995+myro97@users.noreply.github.com>
2024-01-15 21:38:53 +0200

    Add 6. C# Web\1. ASP.NET Fundamentals\
    Add 5.State Management _ Asynchronous Processing/AsynchronousProcessing/
    -> Add 7.EvenNumbersThread

---
+++ 6. C# Web/1. ASP.NET Fundamentals/5.State Management _ Asynchronous Processing/AsynchronousProcessing/7.EvenNumbersThread/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _7.EvenNumbersThread;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int startNum = int.Parse(Console.ReadLine()!);
+        int endNum = int.Parse(Console.ReadLine()!);
+
+        Thread evens = new(() => PrintEvenNumbers(startNum, endNum));
+        evens.Start();
+        evens.Join();
+
+        Console.WriteLine("Thread finish work");
+    }
+    private static void PrintEvenNumbers(int startNum, int endNum)
+    {
+        for(int i = startNum; i <= endNum; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
