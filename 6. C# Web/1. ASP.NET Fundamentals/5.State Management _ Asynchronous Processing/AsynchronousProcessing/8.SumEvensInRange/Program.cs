9e0b0b48a76bbadb1f285943545f5b6601411a95
Mariyan <116071995+myro97@users.noreply.github.com>
2024-01-15 21:53:31 +0200

    Update 6. C# Web\1. ASP.NET Fundamentals\
    Update 5.State Management _ Asynchronous Processing/AsynchronousProcessing/
    -> Add 8.SumEvensInRange

---
+++ 6. C# Web/1. ASP.NET Fundamentals/5.State Management _ Asynchronous Processing/AsynchronousProcessing/8.SumEvensInRange/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _8.SumEvensInRange;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string command;
+        while ((command = Console.ReadLine()!) != "end")
+        {
+            if (command == "show")
+            {
+                long result = SumAsync();
+                Console.WriteLine(result);
+            }
+        }
+    }
+
+    private static long SumAsync()
+        => Task.Run(() =>
+        {
+            long sum = 0;
+            for (long i = 1; i < 1000000000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }).Result;
+}
