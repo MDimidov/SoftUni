dce576630012e993711879a6ba0a4690fb319287
Mariyan <116071995+myro97@users.noreply.github.com>
2024-01-19 19:51:09 +0200

    Update 6. C# Web\1. ASP.NET Fundamentals\
    Add 7.Exercise ASP.NET Core Introduction\MVCIntroDemo
    -> Add 1.MVCIntroDemo
    - Update HomeController
    - Add About.cshtml
    - Add Numbers.cshtml
    - Add NumbersToN.cshtml
    - Update _Layout.cshtml
    - Add ProductController
    - Add ViewModels/Product/ProductViewModel.cs
    - Add Views/Product/All.cshtml
    - Add Views/Product/ById.cshtml
    - Add AllAsJson
    - Add AllAsText
    - Add AllAsTextFile
    - Update All Route to My-products

---
+++ 6. C# Web/1. ASP.NET Fundamentals/7.Exercise ASP.NET Core Introduction/MVCIntroDemo/1.MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using _1.MVCIntroDemo.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace _1.MVCIntroDemo.Controllers;
+
+public class ProductController : Controller
+{
+	private IEnumerable<ProductViewModel> products = new HashSet<ProductViewModel>()
+	{
+		new()
+		{
+			Id = 1,
+			Name = "Cheese",
+			Price = 7.0m
+		},
+		new()
+		{
+			Id = 2,
+			Name = "Ham",
+			Price = 5.5m
+		},
+		new()
+		{
+			Id = 3,
+			Name = "Bread",
+			Price = 1.5m
+		},
+	};
+
+	//public IActionResult Index()
+	//{
+	//    return View();
+	//}
+
+	[Route("My-products")]
+	public IActionResult All()
+	{
+		return View(products);
+	}
+
+	public IActionResult ById(int id)
+	{
+		var product = products
+			.FirstOrDefault(p => p.Id == id);
+		if (product == null)
+		{
+			return BadRequest();
+		}
+
+		return View(product);
+	}
+
+	public IActionResult AllAsJson()
+	{
+		var options = new JsonSerializerOptions
+		{
+			WriteIndented = true,
+		};
+
+		return Json(products, options);
+	}
+
+	public IActionResult AllAsText()
+	{
+		var text = String.Empty;
+		foreach (var product in products)
+		{
+			text += $"Product {product.Id}: {product.Name} - {product.Price} lv.{Environment.NewLine}";
+		}
+
+		return Content(text);
+	}
+
+	public IActionResult AllAsTextFile()
+	{
+		StringBuilder sb = new();
+		foreach (var product in products)
+		{
+			sb.AppendLine($"Product {product.Id}: {product.Name} - {product.Price} lv.");
+		}
+
+		Response.Headers.Add(HeaderNames.ContentDisposition, 
+			@"attachmet;filename=products.txt");
+
+		return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
+	}
+}
---
+++ 6. C# Web/1. ASP.NET Fundamentals/7.Exercise ASP.NET Core Introduction/MVCIntroDemo/1.MVCIntroDemo/ViewModels/Product/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.MVCIntroDemo.ViewModels.Product;
+
+public class ProductViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+}
