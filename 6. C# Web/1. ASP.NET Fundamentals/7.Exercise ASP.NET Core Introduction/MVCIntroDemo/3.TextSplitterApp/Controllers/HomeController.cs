7e59a3f25c6d100befe399e2a130041b309fbf80
Mariyan <116071995+myro97@users.noreply.github.com>
2024-01-22 19:41:22 +0200

    Update 6. C# Web\1. ASP.NET Fundamentals\
    Update 7.Exercise ASP.NET Core Introduction\MVCIntroDemo
    -> Add 3.TextSplitterApp
    - Update HomeController
    - Update Index()
    - Add ViewModels/TextViewModel.cs
    - Update Views/Home/Index

---
+++ 6. C# Web/1. ASP.NET Fundamentals/7.Exercise ASP.NET Core Introduction/MVCIntroDemo/3.TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using _3.TextSplitterApp.Models;
+using _3.TextSplitterApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _3.TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            //if (string.IsNullOrWhiteSpace(model.Text))
+            //{
+            //    return RedirectToAction("Index", String.Empty);
+            //}
+
+            string[] splitTextArr = model.Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = string.Join(Environment.NewLine, splitTextArr);
+
+            return RedirectToAction("Index", model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 6. C# Web/1. ASP.NET Fundamentals/7.Exercise ASP.NET Core Introduction/MVCIntroDemo/3.TextSplitterApp/ViewModels/TextViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace _3.TextSplitterApp.ViewModels;
+
+public class TextViewModel
+{
+    [Required(ErrorMessage = "The Text field is required")]
+    [StringLength(30, MinimumLength = 2, 
+        ErrorMessage = "The field Text must be a string with a minimum length of 2 and maximum length of 30.")]
+    public string Text { get; set; } = null!;
+
+    public string SplitText { get; set; } = null!;
+}
