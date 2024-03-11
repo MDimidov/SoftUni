16b9d7bff2ccb25bbfe555f24460640f0dd28cd5
Mariyan <116071995+myro97@users.noreply.github.com>
2024-03-11 19:21:25 +0200

    Update SoftUni\6. C# Web\2. ASP.NET Advanced\
    Update 4.Workshop Project Fundamentals\HouseRentingSystem
    
    - Add view form for adding house. Add service for retrieving all existing categories.

---
+++ 6. C# Web/2. ASP.NET Advanced/4.Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Services.Data/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using HouseRentingSystem.Data;
+using HouseRentingSystem.Services.Data.Interfaces;
+using HouseRentingSystem.Web.ViewModels.Category;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Services.Data;
+
+public class CategoryService : ICategoryService
+{
+    private readonly HouseRentingDbContext dbContext;
+
+    public CategoryService(HouseRentingDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<HouseSelectCategoryFormModel>> AllCategoriesAsync()
+        => await dbContext
+            .Categories
+            .AsNoTracking()
+            .Select(c => new HouseSelectCategoryFormModel
+            {
+                Id = c.Id,
+                Name = c.Name,
+            })
+            .ToArrayAsync();
+}
--- 6. C# Web/2. ASP.NET Advanced/4.Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Controllers/HouseController.cs
+++ 6. C# Web/2. ASP.NET Advanced/4.Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Controllers/HouseController.cs
@@ -1,14 +1,48 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HouseRentingSystem.Services.Data.Interfaces;
+using HouseRentingSystem.Web.Infrastructure.Extensions;
+using HouseRentingSystem.Web.ViewModels.House;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static HouseRentingSystem.Common.NotificationMessageConstants;
 
 namespace HouseRentingSystem.Web.Controllers;
 
 [Authorize]
 public class HouseController : Controller
 {
+	private readonly ICategoryService categoryService;
+	private readonly IAgentService agentService;
+
+	public HouseController(
+		ICategoryService categoryService,
+		IAgentService agentService)
+	{
+		this.categoryService = categoryService;
+		this.agentService = agentService;
+	}
+
 	[AllowAnonymous]
 	public async Task<IActionResult> All()
 	{
 		return View();
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> Add()
+	{
+		bool isAgent = await agentService.AgentExistByUserIdAsync(User.GetId()!);
+
+		if(!isAgent)
+		{
+			this.TempData[ErrorMessage] = "You must become an agent in order to add new houses!";
+			return RedirectToAction("Become", "Agent");
+		}
+
+		HouseFormModel model = new HouseFormModel()
+		{
+			Categories = await categoryService.AllCategoriesAsync()
+		};
+
+		return View(model);
 	}
 }
