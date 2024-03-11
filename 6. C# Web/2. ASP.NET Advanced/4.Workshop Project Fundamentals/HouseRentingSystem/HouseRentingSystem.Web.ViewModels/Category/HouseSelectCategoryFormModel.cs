457ea3ef57ead2564bde773f91c3d1079471b3bb
Mariyan <116071995+myro97@users.noreply.github.com>
2024-03-11 18:47:53 +0200

    Update SoftUni\6. C# Web\2. ASP.NET Advanced\
    Update 4.Workshop Project Fundamentals\HouseRentingSystem
    
    - Created add house form model and validations for adding new house

---
+++ 6. C# Web/2. ASP.NET Advanced/4.Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Web.ViewModels/Category/HouseSelectCategoryFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Web.ViewModels.Category;
+
+public class HouseSelectCategoryFormModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
---
+++ 6. C# Web/2. ASP.NET Advanced/4.Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Web.ViewModels/House/HouseFormModel.cs
@@ -0,0 +1,36 @@
+﻿#nullable disable
+
+using HouseRentingSystem.Web.ViewModels.Category;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Common.EntityValidationConstants.House;
+
+namespace HouseRentingSystem.Web.ViewModels.House;
+
+public class HouseFormModel
+{
+
+    [Required]
+    [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+    public string Title { get; set; }
+
+    [Required]
+    [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+    public string Address { get; set; }
+
+    [Required]
+    [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+    public string Description { get; set; }
+
+    [Required]
+    [StringLength(ImageUrlMaxLength)]
+    [Display(Name = "Image link")]
+    public string ImageUrl { get; set; }
+
+    [Range(typeof(decimal), PricePerMonthMinRange, PricePerMonthMaxRange)]
+    [Display(Name = "Monthly price")]
+    public decimal PricePerMonth { get; set; }
+
+    [Display(Name = "Category")]
+    public int CategoryId { get; set; }
+    public IEnumerable<HouseSelectCategoryFormModel> Categories { get; set; } = new HashSet<HouseSelectCategoryFormModel>();
+}
