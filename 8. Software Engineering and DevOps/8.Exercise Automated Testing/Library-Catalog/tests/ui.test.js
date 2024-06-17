4e2c81dd17792005c3d2080a5a0fcf05a3e87080
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-06-17 15:04:00 +0300

    Update 8.Exercise Automated Testing\Library-Catalog
    - Add test scipt for playwright (test:ui-debug)
    - Add Test - Verify All Books link is visible

--- 8. Software Engineering and DevOps/8.Exercise Automated Testing/Library-Catalog/tests/ui.test.js
+++ 8. Software Engineering and DevOps/8.Exercise Automated Testing/Library-Catalog/tests/ui.test.js
@@ -2,5 +2,18 @@ const {test, expect} = require('@playwright/test');
 const url = "http://localhost:3000";
 
 test('Verify All Books link is visible', async ({page}) =>{
-    await page.goto(url)
-})
+    // Open the application
+    await page.goto(url);
+
+    // Locate page toolbar
+    await page.waitForSelector('#site-header > nav > section > a');
+
+    // Get All Books link
+    const allBooksLink = await page.$('a[href="/catalog"]');
+
+    // Check if element is visible
+    const isElementVisible = await allBooksLink.isVisible();
+
+    //Verify if the element is visible
+    expect(isElementVisible).toBe(true);
+}) 
