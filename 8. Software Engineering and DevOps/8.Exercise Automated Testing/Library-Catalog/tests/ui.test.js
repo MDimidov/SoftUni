51ea386512b6bab510299197c457e803d5584e99
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-06-17 15:54:03 +0300

    Update 8.Exercise Automated Testing\Library-Catalog
    Add Tests:
    - Verify Log In button is visible
    - Verify Register button is visible
    - Verify Register link text
    - Verify valid user can log in

--- 8. Software Engineering and DevOps/8.Exercise Automated Testing/Library-Catalog/tests/ui.test.js
+++ 8. Software Engineering and DevOps/8.Exercise Automated Testing/Library-Catalog/tests/ui.test.js
@@ -16,4 +16,83 @@ test('Verify All Books link is visible', async ({page}) =>{
 
     //Verify if the element is visible
     expect(isElementVisible).toBe(true);
-}) 
+});
+
+test('Verify Log In button is visible', async ({page}) =>{
+    // Open the application
+    await page.goto(url);
+
+    // Locate page toolbar
+    await page.waitForSelector('nav.navbar');
+
+    // Get Log In link
+    const loginLink = await page.$('a[href="/login"]');
+
+    // Check if element is visible
+    const isElementVisible = await loginLink.isVisible();
+
+    //Verify if the element is visible
+    expect(isElementVisible).toBe(true);
+});
+
+test('Verify Register button is visible', async ({page}) =>{
+    // Open the application
+    await page.goto(url);
+
+    // Locate page toolbar
+    await page.waitForSelector('nav.navbar');
+
+    // Get Register link
+    const registerLink = await page.$('a[href="/register"]');
+
+    // Check if element is visible
+    const isElementVisible = await registerLink.isVisible();
+
+    //Verify if the element is visible
+    expect(isElementVisible).toBe(true);
+});
+
+
+test('Verify Register link text', async ({page}) =>{
+    // Open the application
+    await page.goto(url);
+
+    // Locate page toolbar
+    await page.waitForSelector('nav.navbar');
+
+    // Get Register link
+    const registerLink = await page.$('a[href="/register"]');
+
+    // Get element text
+    const elementText = await registerLink.textContent();
+
+    //Verify if the element is visible
+    expect(elementText).toEqual("Register");
+});
+
+test('Verify valid user can log in', async ({page}) =>{
+    // Open the application
+    await page.goto(url);
+
+    // Locate page toolbar
+    await page.waitForSelector('nav.navbar');
+
+    // Get Log In link
+    const loginLink = await page.$('a[href="/login"]');
+    await loginLink.click();
+
+    await page.fill('#email', 'peter@abv.bg');
+    await page.fill('#password', '123456');
+
+    // Log In with account
+    const loginButton = await page.$('#login-form > fieldset > input');
+    await loginButton.click();
+
+    const logoutButton = await page.$('#logoutBtn');
+
+    // Check if element is visible
+    const isElementVisible = await logoutButton.isVisible();
+
+    //Verify if the element is visible
+    expect(isElementVisible).toBe(true);
+});
