f15f2949ae7e04c2dbc2ef6d57bb6a273e2fb572
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-11-14 17:12:40 +0200

    Update 4. JS Front-End\2. JS Front-End\
    4. Exercise JS Syntax Fundamentals\exercises\02. Vacation
    
    Add second method

--- 4. JS Front-End/2. JS Front-End/4. Exercise JS Syntax Fundamentals/exercises/02. Vacation.js
+++ 4. JS Front-End/2. JS Front-End/4. Exercise JS Syntax Fundamentals/exercises/02. Vacation.js
@@ -47,3 +47,71 @@ function VacationPrice(groupCount, groupType, dayOfWeek) {
 }
 
 VacationPrice(40, "Regular", "Saturday")
+
+
+// --------------Variant 2---------------------
+
+
+function VacationPrice(groupCount, groupType, dayOfWeek) {
+    let price = 0;
+    let discount = 1;
+
+    if (groupType === "Students") {
+        switch (dayOfWeek) {
+            case "Friday":
+                price = 8.45;
+                break;
+            case "Saturday":
+                price = 9.8;
+                break;
+            case "Sunday":
+                price = 10.46;
+                break;
+        }
+
+        if(groupCount >= 30){
+            discount -= 0.15; 
+        }
+
+    } else if (groupType === "Business") {
+        switch (dayOfWeek) {
+            case "Friday":
+                price = 10.9;
+                break;
+            case "Saturday":
+                price = 15.6;
+                break;
+            case "Sunday":
+                price = 16;
+                break;
+        }
+
+        if(groupCount >= 100){
+            groupCount -= 10; 
+        }
+
+    } else if (groupType === "Regular") {
+        switch (dayOfWeek) {
+            case "Friday":
+                price = 15.0;
+                break;
+            case "Saturday":
+                price = 20.0;
+                break;
+            case "Sunday":
+                price = 22.5;
+                break;
+        }
+
+        if(groupCount >= 10 && groupCount <= 20){
+            discount -= 0.05; 
+        }
+        
+    }
+
+    const totalPrice = price * discount * groupCount;
+
+    console.log(`Total price: ${totalPrice.toFixed(2)}`)
+}
+
+VacationPrice(30, "Students", "Sunday")
