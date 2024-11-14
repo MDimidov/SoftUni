42db85a3d0d4bb8187c452e93b127557a3f1e25b
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-11-14 16:37:19 +0200

    Update 4. JS Front-End\2. JS Front-End\
    4. Exercise JS Syntax Fundamentals\exercises\01. Ages
    
    Add second method

--- 4. JS Front-End/2. JS Front-End/4. Exercise JS Syntax Fundamentals/exercises/01. Ages.js
+++ 4. JS Front-End/2. JS Front-End/4. Exercise JS Syntax Fundamentals/exercises/01. Ages.js
@@ -31,4 +31,45 @@ function AgeType(age) {
     console.log(result);
 }
 
+
 AgeType(20);
+
+
+// --------------Variant 2---------------------
+
+function AgeTypes(age){
+
+    const baby = "baby";
+    const child = "child";
+    const teenager = "teenager";
+    const adult = "adult";
+    const elder = "elder";
+
+    let result = "out of bounds";
+
+    switch (true) {
+        case (age >= 66):
+            result = elder;
+            break;
+
+        case age >= 20:
+            result = adult;
+            break;
+    
+        case age >= 14:
+            result = teenager;
+            break;
+
+        case age >= 3:
+            result = child;
+            break;
+
+        case age >= 0:
+            result = baby;
+            break;
+    }
+
+    console.log(result);
+}
+
+AgeTypes(69);
