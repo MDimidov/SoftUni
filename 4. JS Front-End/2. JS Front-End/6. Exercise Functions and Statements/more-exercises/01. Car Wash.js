e6466285d31514fad25e58b59c906251343070e0
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-11-20 11:39:49 +0200

    Update 4. JS Front-End\2. JS Front-End\
    6. Exercise Functions and Statements\more-exercises\
    
    - 01. Car Wash.js
    
    Add second methods

--- 4. JS Front-End/2. JS Front-End/6. Exercise Functions and Statements/more-exercises/01. Car Wash.js
+++ 4. JS Front-End/2. JS Front-End/6. Exercise Functions and Statements/more-exercises/01. Car Wash.js
@@ -19,3 +19,28 @@ function carWash(array) {
 }
 
 carWash(["soap", "water", "mud", "mud", "water", "mud", "vacuum cleaner"]);
+
+// -----------------Method 2--------------------
+
+function carWash(array) {
+  clean = 0;
+
+  const commands = {
+    soap: 10,
+    water: 0.2,
+    "vacuum cleaner": 0.25,
+    mud: -0.1,
+  }
+  
+  array.forEach(element => {
+    if(element === "soap"){
+      clean += commands[element];
+    } else{
+      clean += clean * commands[element];
+    }
+  });
+  
+  console.log(`The car is ${clean.toFixed(2)}% clean.`)
+}
+
+carWash(["soap", "water", "mud", "mud", "water", "mud", "vacuum cleaner"]);
