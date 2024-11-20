bff0b932431e8d3647a5652243e02bc340d348eb
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-11-20 11:16:06 +0200

    Update 4. JS Front-End\2. JS Front-End\
    6. Exercise Functions and Statements\exercises\10. Factorial Division.js
    
    Add second methods

--- 4. JS Front-End/2. JS Front-End/6. Exercise Functions and Statements/exercises/10. Factorial Division.js
+++ 4. JS Front-End/2. JS Front-End/6. Exercise Functions and Statements/exercises/10. Factorial Division.js
@@ -14,3 +14,21 @@ function factorialDevision(a, b) {
 }
 
 factorialDevision(6, 2);
+
+// -----------------Method 2--------------------
+
+function factorialDevision(a, b) {  
+  function getFactorial(num) {
+    let factorial = 1;
+
+    for (let i = 2; i <= num; i++) {
+      factorial *= i;
+    }
+
+    return factorial
+  }
+  
+  console.log((getFactorial(a) / getFactorial(b)).toFixed(2));
+}
+
+factorialDevision(5, 2);
