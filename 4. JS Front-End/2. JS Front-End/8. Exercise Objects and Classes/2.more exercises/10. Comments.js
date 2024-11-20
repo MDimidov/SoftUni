d543875d5927d066f3a459b2b359ed258c2470b2
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-11-20 15:03:01 +0200

    Update 4. JS Front-End\2. JS Front-End
    8. Exercise Objects and Classes\2.more exercises\
    
    Fixed:
    - 10. Comments.js
    - 12. SoftUni Students.js

--- 4. JS Front-End/2. JS Front-End/8. Exercise Objects and Classes/2.more exercises/10. Comments.js
+++ 4. JS Front-End/2. JS Front-End/8. Exercise Objects and Classes/2.more exercises/10. Comments.js
@@ -56,14 +56,108 @@ function showComments(inputArr) {
     });
 }
 
+// showComments([
+//   "user Mark",
+//   "Mark posts on someArticle: NoTitle, stupidComment",
+//   "article Bobby",
+//   "article Steven",
+//   "user Liam",
+//   "user Henry",
+//   "Mark posts on Bobby: Is, I do really like them",
+//   "Mark posts on Steven: title, Run",
+//   "someUser posts on Movies: Like",
+// ]);
+
+// -----------------Method 2--------------------
+
+function showComments(inputArr) {
+  const users = [];
+  const articles = [];
+  const comments = [];
+
+  inputArr.forEach((command) => {
+    if (command.includes('user')) {
+      addUser(command);
+    } else if (command.includes('article')) {
+      addArticle(command);
+    } else if (command.includes('posts on')) {
+      const commentInfo = getCommentInfo(command);
+      if (isUserAndArticleValid(commentInfo.user, commentInfo.article)) {
+        addComment(commentInfo);  
+      }
+    }
+  });
+  
+  function addUser(userCommand) {
+    const [_, userName] = userCommand.split('user ');
+    users.push(userName);
+  }
+
+  function addArticle(articleCommand) {
+    const [_, article] = articleCommand.split('article ');
+    articles.push(article);
+  }
+
+  function getCommentInfo(commentCommand) {
+  const [user, comments] = commentCommand.split(' posts on ');
+  const [article, comment] = comments.split(': ');
+  const [commentTitle, commentContent] = comment.split(', ');
+
+  return {
+    user,
+    article,
+    commentTitle,
+    commentContent,
+  };
+  }
+
+  function isUserAndArticleValid(user, article) {
+    return users.includes(user) && articles.includes(article);
+  }
+
+  function addComment(commentInfo) {
+    comments.push(commentInfo);
+  }
+  
+  // Групиране на коментарите по статии
+  const groupedComments = comments.reduce((acc, comment) => {
+    if (!acc[comment.article]) {
+      acc[comment.article] = [];
+    }
+    acc[comment.article].push(comment);
+    return acc;
+  }, {});
+
+  // Сортиране на статиите по брой коментари (низходящо)
+
+  const sortedArticles = Object.entries(groupedComments)
+    .sort((a, b) => b[1].length - a[1].length)
+    .map(([article, comments]) => ({
+      article,
+      comments: comments.sort((a, b) => a.user.localeCompare(b.user)), // Сортиране на коментарите по име на потребителя
+    }));
+
+
+
+  // Форматиране и извеждане на резултата
+
+  sortedArticles.forEach(({ article, comments }) => {
+    console.log(`Comments on ${article}`);
+    comments.forEach(({ user, commentTitle, commentContent }) => {
+      console.log(`--- From user ${user}: ${commentTitle} - ${commentContent}`);
+    });
+  });
+}
+
 showComments([
-  "user Mark",
-  "Mark posts on someArticle: NoTitle, stupidComment",
-  "article Bobby",
-  "article Steven",
-  "user Liam",
-  "user Henry",
-  "Mark posts on Bobby: Is, I do really like them",
-  "Mark posts on Steven: title, Run",
-  "someUser posts on Movies: Like",
+  'user aUser123', 
+  'someUser posts on someArticle: NoTitle, stupidComment', 
+  'article Books',
+  'article Movies', 
+  'article Shopping', 'user someUser', 
+  'user uSeR4', 'user lastUser', 
+  'uSeR4 posts on Books: I like books, I do really like them', 
+  'uSeR4 posts on Movies: I also like movies, I really do', 
+  'someUser posts on Shopping: title, I go shopping every day',
+  'someUser posts on Movies: Like, I also like movies very much'
 ]);
--- 4. JS Front-End/2. JS Front-End/8. Exercise Objects and Classes/2.more exercises/12. SoftUni Students.js
+++ 4. JS Front-End/2. JS Front-End/8. Exercise Objects and Classes/2.more exercises/12. SoftUni Students.js
@@ -80,16 +80,127 @@ function showCourses(inputArr) {
     });
 }
 
+// showCourses([
+//   "JavaBasics: 15",
+//   "user1[26] with email user1@user.com joins JavaBasics",
+//   "user2[36] with email user11@user.com joins JavaBasics",
+//   "JavaBasics: 5",
+//   "C#Advanced: 5",
+//   "user1[26] with email user1@user.com joins C#Advanced",
+//   "user2[36] with email user11@user.com joins C#Advanced",
+//   "user3[6] with email user3@user.com joins C#Advanced",
+//   "C#Advanced: 1",
+//   "JSCore: 8",
+//   "user23[62] with email user23@user.com joins JSCore",
+// ]);
+
+// -----------------Method 2--------------------
+
+function showCourses(inputArr) {
+  let courses = [];
+
+  inputArr.forEach((command) => {
+    if (command.includes('with')) {
+      const studentInfo = getStudentInfo(command);
+      if (checkIfCourseExist(studentInfo.courseName)) {
+        addStudentToCourse(studentInfo);
+      }
+    } else if (command.includes(':')) {
+      addCourseOrIncreaseCapacity(command);
+    }
+  });
+
+  function checkIfCourseExist(courseName) {
+    return courses.find((c) => c.courseName === courseName);
+  }
+
+  function getStudentInfo(studentCommand) {
+    const [username, command] = studentCommand.split('[');
+    const [credits, lastCommand] = command.split('] with email ');
+    const [email, courseName] = lastCommand.split(' joins ');
+
+    return {
+      username,
+      credits: Number(credits),
+      email,
+      courseName,
+    };
+  }
+
+  function addStudentToCourse(studentInfo) {
+    const course = checkIfCourseExist(studentInfo.courseName);
+    const student = {
+      username: studentInfo.username,
+      credits: studentInfo.credits,
+      email: studentInfo.email,
+    };
+    
+    if (!course.students) {
+      course.students = [];
+    }
+
+    if (course.capacity > 0) {
+      course.students.push(student);
+      course.capacity--;
+    }
+  }
+
+  function addCourseOrIncreaseCapacity(courseCommand) {
+    const [courseName, capacityStr] = courseCommand.split(': ');
+    const capacity = Number(capacityStr.split(' places left')[0]);
+    
+    const course = checkIfCourseExist(courseName);
+    if (course) {
+      course.capacity += capacity;
+    } else {
+      courses.push({
+        courseName: courseName,
+        capacity: capacity,
+        students: [],
+      });
+    }
+  }
+
+  courses
+  .sort((a, b) => b.students.length - a.students.length)
+  .forEach((course) => {
+    console.log(`${course.courseName}: ${course.capacity} places left`);
+    course.students
+    .sort((a, b) => b.credits - a.credits)
+    .forEach((student) => {
+      console.log(`--- ${student.credits}: ${student.username}, ${student.email}`);
+    });
+  });
+}
+
+// showCourses([
+//   "JavaBasics: 15",
+//   "user1[26] with email user1@user.com joins JavaBasics",
+//   "user2[36] with email user11@user.com joins JavaBasics",
+//   "JavaBasics: 5",
+//   "C#Advanced: 5",
+//   "user1[26] with email user1@user.com joins C#Advanced",
+//   "user2[36] with email user11@user.com joins C#Advanced",
+//   "user3[6] with email user3@user.com joins C#Advanced",
+//   "C#Advanced: 1",
+//   "JSCore: 8",
+//   "user23[62] with email user23@user.com joins JSCore",
+// ]);
+
 showCourses([
-  "JavaBasics: 15",
-  "user1[26] with email user1@user.com joins JavaBasics",
-  "user2[36] with email user11@user.com joins JavaBasics",
-  "JavaBasics: 5",
-  "C#Advanced: 5",
-  "user1[26] with email user1@user.com joins C#Advanced",
-  "user2[36] with email user11@user.com joins C#Advanced",
-  "user3[6] with email user3@user.com joins C#Advanced",
-  "C#Advanced: 1",
-  "JSCore: 8",
-  "user23[62] with email user23@user.com joins JSCore",
+  'JavaBasics: 2', 
+  'user1[25] with email user1@user.com joins C#Basics', 
+  'C#Advanced: 3', 
+  'JSCore: 4', 
+  'user2[30] with email user2@user.com joins C#Basics',
+  'user13[50] with email user13@user.com joins JSCore',
+  'user1[25] with email user1@user.com joins JSCore', 
+  'user8[18] with email user8@user.com joins C#Advanced',
+  'user6[85] with email user6@user.com joins JSCore', 
+  'JSCore: 2',
+  'user11[3] with email user11@user.com joins JavaBasics',
+  'user45[105] with email user45@user.com joins JSCore',
+  'user007[20] with email user007@user.com joins JSCore',
+  'user700[29] with email user700@user.com joins JSCore',
+  'user900[88] with email user900@user.com joins JSCore',
 ]);
