6c68903478cf89e5fdaef2f425e53dfece12c33c
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-11-26 15:01:05 +0200

    Update 4. JS Front-End\2. JS Front-End
    10. Exercise DOM Introduction\
    
    - 09. Number Convertor
    
    Add second method

--- 4. JS Front-End/2. JS Front-End/10. Exercise DOM Introduction/09. Number Convertor/solution.js
+++ 4. JS Front-End/2. JS Front-End/10. Exercise DOM Introduction/09. Number Convertor/solution.js
@@ -26,3 +26,33 @@ function solve() {
     }
 
 }
+
+// -----------------Method 2--------------------
+
+function solve() {
+    const button = document.querySelector('#container>button');
+    button.onclick = onClick;
+
+    const menu = document.querySelector("#selectMenuTo");
+    const option = document.createElement("option");
+    const option2 = document.createElement("option");
+    option.text = "Binary";
+    option.value = "binary";
+    option2.text = "Hexadecimal";
+    option2.value = "hexadecimal";
+    menu.appendChild(option);
+    menu.appendChild(option2);
+    
+
+    function onClick() {
+        const inputNum = Number(document.querySelector('#input').value);
+        const selectedTransfer = Array.from(document.querySelectorAll('#selectMenuTo>option')).find(o => o.selected);
+        const result = document.querySelector('#result');
+        
+        if (selectedTransfer === option) {
+            result.value = inputNum.toString(2);
+        } else if (selectedTransfer === option2) {
+            result.value = inputNum.toString(16).toUpperCase();
+        }
+    }
+}
