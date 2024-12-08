45e1e1b7ccbfb17093cf2bc982ae82ea9382d36e
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-12-08 20:24:49 +0200

    Update 04. Github-Commits

--- 4. JS Front-End/2. JS Front-End/15. HTTP and REST/04. Github-Commits/app.js
+++ 4. JS Front-End/2. JS Front-End/15. HTTP and REST/04. Github-Commits/app.js
@@ -1,4 +1,28 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.querySelector("#username").value;
+    const repoName = document.querySelector("#repo").value;
+	const url = `https://api.github.com/repos/${username}/${repoName}/commits`;
+	const commitsList = document.querySelector("#commits");
+	commitsList.innerHTML = '';
+
+    fetch(url)
+        .then((response) => {
+            if(!response.ok){
+				throw new Error(`User not found: ${response.status}`);
+			}
+			
+			return response.json()
+        })
+        .then((data) => {
+            for (const commitInfo of data) {
+                const commitItem = document.createElement("li");
+                commitItem.textContent = `${commitInfo.commit.author.name}: ${commitInfo.commit.message}`;
+                commitsList.appendChild(commitItem);
+            }
+        })
+        .catch((error) => {
+            const commitItem = document.createElement("li");
+                commitItem.textContent = `${error}`;
+                commitsList.appendChild(commitItem);
+        });
 }
