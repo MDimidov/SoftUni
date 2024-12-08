51de9f870a7d6fae98dbb3a8ddc8e599fc059e17
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-12-08 19:47:07 +0200

    Update 4. JS Front-End\2. JS Front-End\
    Update 15. HTTP and REST\02. GitHub-Repos

--- 4. JS Front-End/2. JS Front-End/15. HTTP and REST/03. Github-Repos-by-Username/scripts/app.js
+++ 4. JS Front-End/2. JS Front-End/15. HTTP and REST/03. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,40 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.querySelector("#username").value;
+	const url = `https://api.github.com/users/${username}/repos`;
+	const reposList = document.querySelector("#repos");
+	reposList.innerHTML = '';
+
+	fetch(url)
+		.then((response) => {
+			if(!response.ok){
+				throw new Error(`User not found: ${response.status}`);
+			}
+			
+			return response.json()
+		})
+		.then((data) => {
+			if (data.length === 0) {
+				const listItem = document.createElement("li");
+				listItem.textContent = "No repositories found.";
+				reposList.appendChild(listItem);
+				return;
+			}
+
+			for (const repo of data) {
+				const listItem = document.createElement('li');
+				const link = document.createElement('a');
+				
+				link.href = repo.html_url;
+				link.textContent = repo.full_name;
+				link.target = "_blank";
+
+				listItem.appendChild(link);
+				reposList.appendChild(listItem);
+			}
+		})
+		.catch((error) => {
+			const listItem = document.createElement("li");
+      		listItem.textContent = `Error: ${error.message}`;
+      		reposList.appendChild(listItem);
+		});
 }
