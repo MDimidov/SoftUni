765cde13ff0b7118c7d05b60aa2c644a36d27bd7
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-12-08 22:22:23 +0200

    Solve 01.Bus-Stop

--- 4. JS Front-End/2. JS Front-End/16. Asynchronous Programming/01.Bus-Stop/app.js
+++ 4. JS Front-End/2. JS Front-End/16. Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,29 @@
 function getInfo() {
-    // TODO:
+    const stopId = document.querySelector("#stopId").value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    const stopName = document.querySelector('#stopName');
+    const busesList = document.querySelector('#buses');
+    
+    fetch(url)
+    .then((response) => {
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((stop) => {
+            stopName.textContent = stop.name;
+            console.log(stop.buses);
+            busesList.innerHTML = '';
+            Object.entries(stop.buses).forEach(([busId, minutes]) => {
+                
+                const liElement = document.createElement('li');
+                liElement.textContent = `Bus ${busId} arrives in ${minutes} minutes`;
+                busesList.appendChild(liElement);
+            });
+    })
+    .catch(() => {
+        stopName.textContent = 'Error';
+    });
 }
