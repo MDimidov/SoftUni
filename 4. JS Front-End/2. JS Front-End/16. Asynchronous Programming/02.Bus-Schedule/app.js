b308affe50e7e6b18c5907522569ac8e491cfa7b
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-12-09 12:14:06 +0200

    Solve 02.Bus-Schedule

--- 4. JS Front-End/2. JS Front-End/16. Asynchronous Programming/02.Bus-Schedule/app.js
+++ 4. JS Front-End/2. JS Front-End/16. Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,10 +1,39 @@
 function solve() {
+    let stopId;
+    if (!stopId) stopId = 'depot';
+    let stopName;
+    
+    
+    const divInfo = document.querySelector('#info>span.info');
+    const btnDepart = document.querySelector('#depart');
+    const btnArrive = document.querySelector('#arrive');
+    
     function depart() {
-        // TODO: 
-    }
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stopId}`;
+        
+        fetch(url)
+            .then((response) => {
+                if(!response.ok){
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
 
+                return response.json();
+            })
+            .then((stopInfo) => {
+                stopName = stopInfo.name;
+                divInfo.textContent = `Next stop ${stopName}`;
+                stopId = stopInfo.next;
+            });
+            
+        btnDepart.disabled = true;    
+        btnArrive.disabled = false;   
+    }
+    
     async function arrive() {
-        // TODO:
+        divInfo.textContent = `Arriving at ${stopName}`;
+            
+        btnArrive.disabled = true; 
+        btnDepart.disabled = false;    
     }
 
     return {
