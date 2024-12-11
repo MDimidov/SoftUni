4b8cc91e9312c4fa8c642e9461b101c9c0989f8c
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-12-11 13:03:30 +0200

    solve 02.Messenger

--- 4. JS Front-End/2. JS Front-End/17. Exercise HTTP and AJAX/02.Messenger/app.js
+++ 4. JS Front-End/2. JS Front-End/17. Exercise HTTP and AJAX/02.Messenger/app.js
@@ -1,5 +1,50 @@
 function attachEvents() {
-    // TODO:
+    const baseURL = 'http://localhost:3030/jsonstore/messenger';
+    const textarea = document.querySelector('#messages');
+    const btnSubmit = document.querySelector('#submit');
+    const btnRefresh = document.querySelector('#refresh');
+    
+    btnRefresh.addEventListener('click', getMessages);
+    btnSubmit.addEventListener('click', addMessage);
+    
+    function getMessages() {
+        const text = [];
+
+        fetch(baseURL)
+            .then((response) => response.json())
+            .then((messages) => {
+                for (const key of Object.keys(messages)) {
+                    text.push(`${messages[key].author}: ${messages[key].content}`);
+                }
+
+                textarea.textContent = text.join('\n')
+            })
+            .catch((error) => console.error(error));
+    }
+
+    function addMessage() {
+        const authorName = document.querySelector('input[name="author"]');
+        const content = document.querySelector('input[name="content"]');
+        const data = {
+            author: authorName.value,
+            content: content.value
+        };
+
+        fetch(baseURL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify(data)
+        })
+        .catch((error) => console.error(error));
+
+        authorName.value = '';
+        content.value = '';
+
+        // If you want tho refresh messages automatic
+        // btnRefresh.click();
+    }
+
+    btnRefresh.click();
 }
 
 attachEvents();
