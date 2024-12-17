6a19817d0891ff3c7aa31d8cd0d5211309890772
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-12-17 08:37:02 +0200

    Fixed 02.Eco Events

--- 4. JS Front-End/2. JS Front-End/-. Exams/-. JS Front-End Regular Exam - 15 December 2024/02.Eco Events/app.js
+++ 4. JS Front-End/2. JS Front-End/-. Exams/-. JS Front-End Regular Exam - 15 December 2024/02.Eco Events/app.js
@@ -17,7 +17,11 @@ function solve() {
     return element;
 }
 
-  const fields = Array.from(document.querySelectorAll('form>input'));
+  const inputs = {email, event, location};
+  // const fields = Array.from(document.querySelectorAll('form>input'));
+  const emailInput = document.querySelector('#email');
+  const eventInput = document.querySelector('#event');
+  const locationInput = document.querySelector('#location');
   const previewList = document.querySelector('#preview-list');
   const eventList = document.querySelector('#event-list');
   const btnNext = document.querySelector('#next-btn');
@@ -25,54 +29,59 @@ function solve() {
 
   function createEntry(e) {
     e.preventDefault();
-    const [email, event, location] = fields.map(f => f.value);
+    inputs.email = emailInput.value;
+    inputs.event = eventInput.value;
+    inputs.location = locationInput.value;
+    console.log(inputs)
     
-    if (email && event && location) {
-      const entryLi = createElement('li', {className: 'application', dataset: {email, event, location}}, previewList);
+    if (inputs.email && inputs.event && inputs.location) {
+      const entryLi = createElement('li', {className: 'application'}, previewList);
       const article = createElement('article', {}, entryLi);
-      createElement('h4', {textContent: email}, article);
+      createElement('h4', {textContent: inputs.email}, article);
       const p1 = createElement('p', {}, article);
       createElement('strong', {textContent: 'Event:'}, p1);
       createElement('br', {}, p1);
-      p1.innerHTML += event;
+      p1.innerHTML += inputs.event;
 
       const p2 = createElement('p', {}, article);
       createElement('strong', {textContent: 'Location:'}, p2);
       createElement('br', {}, p2);
-      p2.innerHTML += location;
+      p2.innerHTML += inputs.location;
 
       createElement('button', {classList: 'action-btn edit', textContent: 'edit', onclick: editHandler}, entryLi);
       createElement('button', {classList: 'action-btn apply', textContent: 'apply', onclick: applyHandler}, entryLi);
 
       btnNext.disabled = true;
-      fields.forEach(f => f.value = '');
+      // fields.forEach(f => f.value = '');
+      emailInput.value = '';
+      eventInput.value = '';
+      locationInput.value = '';
     }
   }
 
   function editHandler(e) {
-    const li = e.target.parentElement;
-    let input = Object.assign({}, li.dataset);
-    input = Object.values(input);
+    // let input = Object.values(inputs);
 
-    fields.forEach((f, index) => {
-      f.value = input[index];
-    });
+    // fields.forEach((f, index) => {
+    //   f.value = input[index];
+    // });
+
+    emailInput.value = inputs.email;
+    eventInput.value = inputs.event;
+    locationInput.value = inputs.location;
 
     btnNext.disabled = false;
     previewList.innerHTML = '';
   }
 
-  function applyHandler(e) {
-    const li = e.target.parentElement;
-    let eventInfo = Object.assign({}, li.dataset);
-
-    applyEntry(eventInfo);
+  function applyHandler() {
+    applyEntry(inputs);
 
     previewList.innerHTML = '';
   }
 
   function applyEntry({email, event, location}) {
-    const entryLi = createElement('li', {className: 'application', dataset: {email, event, location}}, eventList);
+    const entryLi = createElement('li', {className: 'application'}, eventList);
     const article = createElement('article', {}, entryLi);
     createElement('h4', {textContent: email}, article);
     const p1 = createElement('p', {}, article);
