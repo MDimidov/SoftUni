aee3b57b668b4af5d4793824caa8908dabceff9f
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-01 13:35:42 +0300

    Declare List class in List.h

--- 7. C# DataStructures/1.DataStructuresFundamentals/04. Linear Data Structures/C++/LinearDataStructures/Problem01.List/source/list/List.h
+++ 7. C# DataStructures/1.DataStructuresFundamentals/04. Linear Data Structures/C++/LinearDataStructures/Problem01.List/source/list/List.h
@@ -1,9 +1,40 @@
 #pragma once
-#ifndef List
-#define List
-#endif // !list
+#ifndef LIST_H
+#define LIST_H
 
-class List	
+#include "IAbstractList.h"
+#include "vector"
+#include "stdexcept"
+#include "algorithm"
+
+template <typename T>
+class List	 : public IAbstractList<T>
 {
+private:
+	int DEFAULT_SIZE = 4;
+	std::T[] items;
+public:
+	List() {
+		items = new T[DEFAULT_SIZE];
+	}
+
+	List(int size) {
+		items = new T[size];
+	}
+
+	size_t Count() const override;
+	T& operator[](size_t index) override;
+	const T& operator[](size_t index) const override;
+
+	void Add(const T& item) override;
+	void Insert(size_t index, const T& item) override;
+	bool Contains(const T& item) const override;
+	int IndexOf(const T& item) const override;
+	bool Remove(const T& item) override;
+	void RemoveAt(size_t index) override;
+
 };
 
+#include <List.tpp>
+
+#endif // LIST_H
