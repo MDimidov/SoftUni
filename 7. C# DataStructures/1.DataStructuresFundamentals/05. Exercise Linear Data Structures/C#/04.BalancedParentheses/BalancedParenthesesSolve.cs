e8046a42cc4c2a5285a5c2279d57adf8320a46db
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-23 20:30:11 +0300

    Solved Balanced Parentheses

--- 7. C# DataStructures/1.DataStructuresFundamentals/05. Exercise Linear Data Structures/C#/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ 7. C# DataStructures/1.DataStructuresFundamentals/05. Exercise Linear Data Structures/C#/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -1,13 +1,48 @@
-﻿namespace Problem04.BalancedParentheses
-{
-    using System;
-    using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Problem04.BalancedParentheses
+{
     public class BalancedParenthesesSolve : ISolvable
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            char[] openedBrackets = { '{', '[', '(' };
+            char[] closedBrackets = { '}', ']', ')' };
+
+            Dictionary<char, char> tupleBrackets = new Dictionary<char, char>() {
+                {'}', '{' },
+                {']', '[' },
+                {')', '(' },
+            };
+            Stack<char> brackets = new Stack<char>(parentheses.Length / 2);
+
+            foreach (char c in parentheses)
+            {
+                if (openedBrackets.Contains(c))
+                {
+                    brackets.Push(c);
+                }
+                else if (closedBrackets.Contains(c))
+                {
+                    if (brackets.Any() && tupleBrackets[c] == brackets.Peek())
+                    {
+                        brackets.Pop();
+                    }
+                    else
+                    {
+                        brackets.Push(c);
+                    }
+                }
+            }
+
+            if (brackets.Any())
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
