725a1778d5d3cd47fa9f32007c371cd7121639c8
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-24 11:02:18 +0300

    Solved Reverse Numbers

---
+++ 7. C# DataStructures/1.DataStructuresFundamentals/05. Exercise Linear Data Structures/C#/05.ReverseNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿var nums = Console.ReadLine()!
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse);
+
+Stack<int> stack = new();
+foreach (var num in nums)
+{
+    stack.Push(num);
+}
+
+while (stack.Any())
+{
+    Console.Write(stack.Pop() + " ");
+}
