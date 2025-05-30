6c036e58095bf8b636a5562a25a1ad1094b7aad0
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-30 18:28:27 +0300

    Implement OrderBfs

--- 7. C# DataStructures/1.DataStructuresFundamentals/06. Trees Representation and Traversal (BFS, DFS)/Tree/Program.cs
+++ 7. C# DataStructures/1.DataStructuresFundamentals/06. Trees Representation and Traversal (BFS, DFS)/Tree/Program.cs
@@ -8,8 +8,25 @@
     {
         public static void Main(string[] args)
         {
+            var subtree = new Tree<int>(36,
+                               new Tree<int>(42),
+                               new Tree<int>(3)
+                               );
 
-            
+            var tree = new Tree<int>(34,
+                        new Tree<int>(36,
+                            new Tree<int>(42),
+                                    new Tree<int>(3,
+                                    new Tree<int>(78)
+                                    )
+                                ),
+                                new Tree<int>(1),
+                                new Tree<int>(103)
+                            );
+
+            //Console.WriteLine( string.Join(", ", subtree.OrderBfs()));
+            Console.WriteLine( string.Join(", ", tree.OrderBfs()));
+
         }
     }
 }
--- 7. C# DataStructures/1.DataStructuresFundamentals/06. Trees Representation and Traversal (BFS, DFS)/Tree/Tree.cs
+++ 7. C# DataStructures/1.DataStructuresFundamentals/06. Trees Representation and Traversal (BFS, DFS)/Tree/Tree.cs
@@ -2,16 +2,28 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class Tree<T> : IAbstractTree<T>
     {
+        private T value;
+        private readonly List<Tree<T>> children;  
+        private Tree<T> parent;
+
         public Tree(T value)
         {
+            this.value = value;
+            children = new List<Tree<T>>();
         }
 
         public Tree(T value, params Tree<T>[] children)
             : this(value)
         {
+            foreach(var child in children)
+            {
+                child.parent = this;
+                this.children.Add(child);
+            }
         }
 
         public void AddChild(T parentKey, Tree<T> child)
@@ -21,7 +33,23 @@
 
         public IEnumerable<T> OrderBfs()
         {
-            throw new NotImplementedException();
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+            List<T> result = new List<T>();
+
+            queue.Enqueue(this);
+
+            while (queue.Any())
+            {
+                var subTree = queue.Dequeue();
+                result.Add(subTree.value);
+
+                foreach(var child in subTree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
         }
 
         public IEnumerable<T> OrderDfs()
