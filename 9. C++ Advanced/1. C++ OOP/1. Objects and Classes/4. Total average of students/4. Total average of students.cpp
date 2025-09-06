a6db8de27156fdf99684745a84123afde0d84659
Mariqn Dimidov <mariqn.dimidov@gmail.com>
2025-09-06 19:35:53 +0300

    Add project 4. Total average of students

---
+++ 9. C++ Advanced/1. C++ OOP/1. Objects and Classes/4. Total average of students/4. Total average of students.cpp
@@ -0,0 +1,13 @@
+// 4. Total average of students.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Write a program, use a class that has params:
+// • Student Name
+// • Student Surname
+// • Total Average
+// The class should have print method that for a given object prints all the information. Create a vector in main() that for a given number(passed thru user) saves the objects Make a function that calculates the Total average of all students. If there are no students, print “Invalid input”.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
