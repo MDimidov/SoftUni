c424a98fde6dcb441d9a2af50b93a87654b2f808
Mariqn Dimidov <mariqn.dimidov@gmail.com>
2025-09-06 13:21:34 +0300

    Add project 3. Sales

---
+++ 9. C++ Advanced/1. C++ OOP/1. Objects and Classes/3. Sales/3. Sales.cpp
@@ -0,0 +1,11 @@
+// 3. Sales.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Write a class Sale holding the following data: town, product, price, quantity. Read a list of sales and calculate and print the total sales by the town as shown in the output.Order the towns alphabetically in the output.
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
